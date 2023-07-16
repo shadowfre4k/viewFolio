1621c16e311926d2ecb6dcde672f5831ee3757af
Wave FC <malloriepinto89@gmail.com>
2023-07-16 14:38:40 -0700

    aplpha vantage API

--- public/stocks.js
+++ public/stocks.js
@@ -22,11 +22,16 @@ testButton.addEventListener("click", function (event) {
 //   }
 // };
  
-const getStocks = async () => {
-  let request = 
-      "https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2023-01-09/2023-01-09?apiKey=c5aIUpG8Te084EM9Mh4Irj1shradwA10";
-      console.log(request);
-  let response = await 
-  fetch (request)
-  console.log(response);
-}
+// const getStocks = async () => {
+//   let request = 
+//       "https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=CJD8U5VF6QPBVYDP";
+//       console.log(request);
+//   let response = await 
+//   fetch (request)
+//   console.log(response);
+//   let data = await response.json()
+//   console.log(data);
+//   const topGainers = data.top_gainers[0].change_percentage
+//   const topLosers = data.top_losers[0].change_percentage
+
+// }
