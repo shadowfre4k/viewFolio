958080d56a668f3332a926145cfdea5e4c35546a
LenZuro <126126939+LenZuro@users.noreply.github.com>
2023-07-20 21:26:29 -0700

    front end changes

--- controllers/index.js
+++ controllers/index.js
@@ -4,10 +4,12 @@ const apiRoutes = require("./api");
 const login = require("./login");
 const signUp = require("./signupRoute");
 const news = require("./newsRoute");
+const homepage = require("./homepage")
 
 router.use("/", login);
 router.use("/api", apiRoutes);
 router.use("/signup", signUp);
 router.use("/news", news);
+router.use("/homepage" , homepage);
 
 module.exports = router;
